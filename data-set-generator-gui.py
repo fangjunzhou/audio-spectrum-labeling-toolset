41f4e69b6d13e2d5c55648e8a74a73966021c4b0
Fangjun Zhou <fzhou48@wisc.edu>
2022-08-10 19:30:39 -0500

    Update: Data Set Generator GUI Added spectrum contrast control.

--- data-set-generator-gui.py
+++ data-set-generator-gui.py
@@ -9,6 +9,8 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 
+import numpy as np
+
 from Utils.AudioPlot import AudioMagnitudePlot, AudioSpectrumPlot
 from Utils.AudioProcess import Audio, AudioPlayer
 
@@ -17,6 +19,7 @@ class App(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack(fill=BOTH, expand=True)
+        self.master.protocol("WM_DELETE_WINDOW", self.OnClose)
 
         # =====INITIALIZE=====
 
@@ -35,13 +38,17 @@ class App(tk.Frame):
         self.magFig.tight_layout()
         self.magCanvas = FigureCanvasTkAgg(self.magFig, self)
         self.fftFig, self.fftAx = plt.subplots()
-        self.fftCanvas = FigureCanvasTkAgg(self.fftFig, self)
         self.fftFig.tight_layout()
+        self.fftCanvas = FigureCanvasTkAgg(self.fftFig, self)
         # Audio plot
         self.audioMagnitudePlot = AudioMagnitudePlot(
             self.mainAudio, self.magAx, self.magCanvas)
         self.audioSpectrumPlot = AudioSpectrumPlot(
             self.mainAudio, self.fftAx, self.fftCanvas)
+        
+        self.fttContrastCurveFig, self.fttContrastCurveAx = plt.subplots()
+        self.fttContrastCurveFig.set_size_inches(2, 2)
+        self.fttContrastCurveFig.tight_layout()
 
         # =====FRAMES=====
 
@@ -55,7 +62,7 @@ class App(tk.Frame):
 
         # Left frame for tools
         self.leftFrame = tk.Frame(self)
-        self.leftFrame.pack(side=LEFT)
+        self.leftFrame.pack(side=LEFT, fill=Y)
 
         self.DrawTopFrame()
 
@@ -92,30 +99,48 @@ class App(tk.Frame):
         """
         Method to draw the left frame
         """
+        # Spectrum Frame
+        spectrumFrame = tk.Frame(self.leftFrame)
+        spectrumFrame.pack(side=TOP, fill=tk.BOTH)
+        
         # Label for Spectrogram
         spectrogramLabel = tk.Label(
-            self.leftFrame, text="Spectrogram Settings")
+            spectrumFrame, text="Spectrogram Settings")
         spectrogramLabel.grid(row=0, column=0)
 
         # Slider for Spectrogram brightness enhancement
         brightnessSliderLabel = tk.Label(
-            self.leftFrame, text="Brightness")
+            spectrumFrame, text="Brightness")
         brightnessSliderLabel.grid(row=1, column=0)
         brightnessSlider = tk.Scale(
-            self.leftFrame,
-            from_=1, to=.01, resolution=.01,
+            spectrumFrame,
+            from_=0, to=1, resolution=.001,
             orient=tk.HORIZONTAL,
             command=self.BrightnessSlider)
-        brightnessSlider.set(1)
+        brightnessSlider.set(0)
         brightnessSlider.grid(row=1, column=1)
+        
+        contrastSliderLabel = tk.Label(
+            spectrumFrame, text="Contrast")
+        contrastSliderLabel.grid(row=2, column=0)
+        self.fttContrastCurveCanvas = FigureCanvasTkAgg(self.fttContrastCurveFig, spectrumFrame)
+        self.fttContrastCurveCanvas.draw()
+        self.fttContrastCurveCanvas.get_tk_widget().grid(row=3, column=0, columnspan=2)
+        contrastSlider = tk.Scale(
+            spectrumFrame,
+            from_=1, to=32, resolution=.01,
+            orient=tk.HORIZONTAL,
+            command=self.ContrastSlider)
+        contrastSlider.set(1)
+        contrastSlider.grid(row=4, column=0, columnspan=2)
 
     def DrawBottomFrame(self):
         """
         Method to draw the bottom frame
         """
         playControl = {
-            "play": self.Play,
-            "pause": self.Pause
+            "Play From Start": self.Play,
+            "Stop": self.Pause
         }
         playControlFrame = tk.Frame(self.bottomFrame)
         playControlFrame.pack(side=tk.TOP)
@@ -123,11 +148,6 @@ class App(tk.Frame):
             playControl[key] = tk.Button(
                 playControlFrame, text=key, command=playControl[key])
             playControl[key].pack(side=tk.LEFT)
-
-        toolbarFrame = tk.Frame(self.bottomFrame)
-        toolbarFrame.pack(side=TOP)
-        toolbar = NavigationToolbar2Tk(self.fftCanvas, toolbarFrame)
-        toolbar.update()
 
         # Status bar
         statusBar = tk.Label(self.bottomFrame, textvariable=self.status)
@@ -160,7 +180,7 @@ class App(tk.Frame):
         # Set up audio player
         self.audioPlayer = AudioPlayer(
             self.mainAudio,
-            1,
+            0.2,
             self.UpdateAudioCursor
         )
         
@@ -176,6 +196,20 @@ class App(tk.Frame):
         Method to handle the brightness slider
         """
         self.audioSpectrumPlot.SetBrightnessEnhancement(float(value))
+    
+    def ContrastSlider(self, value):
+        """
+        Method to handle the contrast slider
+        """
+        # Plot f(x) = 1 - (1-x)^(contrast)
+        self.fttContrastCurveAx.clear()
+        self.fttContrastCurveAx.plot(
+            np.linspace(0, 1, 1000),
+            1 - np.power(1 - np.linspace(0, 1, 1000), float(value))
+        )
+        self.fttContrastCurveCanvas.draw()
+        
+        self.audioSpectrumPlot.SetContrastEnhancement(float(value))
 
     def Play(self):
         """
@@ -185,7 +219,8 @@ class App(tk.Frame):
             return
         
         # Play the audio file
-        self.audioPlayer.Play()
+        playThread = threading.Thread(target=self.audioPlayer.Play)
+        playThread.start()
 
     def Pause(self):
         """
@@ -203,6 +238,19 @@ class App(tk.Frame):
         """
         self.audioMagnitudePlot.SetCursorPosition(value)
         self.audioSpectrumPlot.SetCursorPosition(value)
+        
+    def OnClose(self):
+        """
+        Method to exit the program
+        """
+        print("Exiting")
+        # Pause the audio player
+        self.Pause()
+        
+        # Close the window
+        self.quit()
+        self.destroy()
+        exit()
 
 
 # create the application
